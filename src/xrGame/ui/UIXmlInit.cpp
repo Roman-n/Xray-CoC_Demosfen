1e5d7782dc7ae4835764d4f0a1575f4596bf41bf
Romann <nru.2018@yandex.ru>
2018-03-15 01:45:40 +0300

    Рандомные текстуры Реализация рандомныx текстур в тегаx конфигов ui. Спасибо Charsi за помощь.

--- src/xrGame/ui/UIXmlInit.cpp
+++ src/xrGame/ui/UIXmlInit.cpp
@@ -931,39 +931,41 @@ bool CUIXmlInit::InitSleepStatic(CUIXml &xml_doc, const char *path, int index, C
 
 bool CUIXmlInit::InitTexture(CUIXml& xml_doc, LPCSTR path, int index, ITextureOwner* pWnd)
 {
-	string256 buf;
-	LPCSTR texture	= NULL;
-	LPCSTR shader	= NULL;
-	strconcat	(sizeof(buf),buf, path, ":texture");
-	if (xml_doc.NavigateToNode(buf))
+	//<Рандомные текстуры> Romann (спасибо Charsi)
+	int nodes_num = xml_doc.GetNodesNum(path, index, "texture");
+	if (nodes_num > 0)
 	{
-		texture		= xml_doc.Read(buf, index, NULL);
-		shader		= xml_doc.ReadAttrib(buf, index, "shader", NULL);
-	}
-	if (texture)
-	{
-		if(shader)
-			pWnd->InitTextureEx(texture, shader);
-		else
-	       pWnd->InitTexture(texture);
-	}
-//--------------------
-	Frect			rect;
-	rect.x1			= xml_doc.ReadAttribFlt(buf, index, "x", 0);
-	rect.y1			= xml_doc.ReadAttribFlt(buf, index, "y", 0);
-	rect.x2			= rect.x1 + xml_doc.ReadAttribFlt(buf, index, "width", 0);	
-	rect.y2			= rect.y1 + xml_doc.ReadAttribFlt(buf, index, "height", 0);
+		auto root = xml_doc.GetLocalRoot();
+		xml_doc.SetLocalRoot(xml_doc.NavigateToNode(path, index));
 
+		int ind = Random.randI(nodes_num);
+
+		LPCSTR texture = xml_doc.Read("texture", ind, NULL);
+		LPCSTR shader = xml_doc.ReadAttrib("texture", ind, "shader", NULL);
+		if (texture) {
+			if (shader)
+				pWnd->InitTextureEx(texture, shader);
+			else
+				pWnd->InitTexture(texture);
+		}
+
+		Frect rect;
+		rect.x1 = xml_doc.ReadAttribFlt("texture", ind, "x", 0);
+		rect.y1 = xml_doc.ReadAttribFlt("texture", ind, "y", 0);
+		rect.x2 = rect.x1 + xml_doc.ReadAttribFlt("texture", ind, "width", 0);
+		rect.y2 = rect.y1 + xml_doc.ReadAttribFlt("texture", ind, "height", 0);
+		if (rect.width() != 0 && rect.height() != 0) pWnd->SetTextureRect(rect);
+
+		u32 color = GetColor(xml_doc, "texture", ind, 0xff);
+		pWnd->SetTextureColor(color);
+
+		xml_doc.SetLocalRoot(root);
+	}
 	bool stretch_flag = xml_doc.ReadAttribInt(path, index, "stretch") ? true : false;
 	pWnd->SetStretchTexture(stretch_flag);
 
-	u32 color = GetColor(xml_doc, buf, index, 0xff);
-	pWnd->SetTextureColor(color);
-
-	if (rect.width() != 0 && rect.height() != 0)
-		pWnd->SetTextureRect(rect);
-
 	return true;
+	//Romann
 }
 
 bool CUIXmlInit::InitTextureOffset(CUIXml &xml_doc, LPCSTR path, int index, CUIStatic* pWnd){
